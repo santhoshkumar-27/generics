944317b61e681c9a71b4f74103a83225781d8361
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2024-05-27 16:49:04 +0530

    feat: multiple generic parameters

--- helper.ts
+++ helper.ts
@@ -6,6 +6,13 @@ function convertIntoArray<T>(input: T): T[] { // T --> Type parameter
 
 const inlineFunction = <T>(input: T): T[] => [input];
 
+// muliple Parameter generice Types
+function createArrayPair<T, K>(input1: T, input2: K): [T, K] {
+    return [input1, input2];
+}
+
+const inlineArrayPair = <T, K>(input1: T, input2: K): [T, K] => [input1, input2];
+
 convertIntoArray('string'); // string
 convertIntoArray(0); // number
 convertIntoArray(false); // boolean
