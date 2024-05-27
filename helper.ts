02995c0ea5f326b1e61d7ef0f1cde5b7e3950345
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2024-05-27 15:38:24 +0530

    feat: function expression generic

--- helper.ts
+++ helper.ts
@@ -1,8 +1,10 @@
 // as far we don't mentions a type so its by default any
-function convertIntoArray<T>(input: T): T[] { // --> Type parameter
+function convertIntoArray<T>(input: T): T[] { // T --> Type parameter
     // now this function converted into the generic functions because it accepts type as parameter, where it invoked
     return [input];
 }
+
+const inlineFunction = <T>(input: T): T[] => [input];
 
 convertIntoArray('string'); // string
 convertIntoArray(0); // number
